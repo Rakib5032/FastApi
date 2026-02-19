9c3751c22b2913479e5fc37a7c1681b71209c85d
Rakib5032 <rabbi15-5032@diu.edu.bd>
2026-02-19 15:47:54 +0600

    CRUD Operation started

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+
 @app.get("/")
 def hello():
-    return {'message': 'Hello world'}
+    return {'message': 'Patient Mangement System'}
 
 @app.get('/about')
 def about():
-    return {'Message': 'This is about FastApi'}
+    return {'Message': 'Fully functional Patient record system'}
+
+@app.get("/data")
+def data():
+    return load_data()
